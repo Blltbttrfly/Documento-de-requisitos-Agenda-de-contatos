744e4eb46fbdf2f64c28ecb6d047dff98aa84772
LayLeo <layusais23+tds23@gmail.com>
2026-07-01 21:23:12 -0300

    update crud

--- Requisitos agenda de contatos/main.py
+++ Requisitos agenda de contatos/main.py
@@ -1,35 +1,8 @@
-#RF01 cadastrar contato
-
-class Contato:
-    def __init__(self, nome, telefone, nacionalidade, email, vinculo, aniversario):
-        self.nome = nome    
-        self.telefone = telefone
-        self.nacionalidade = nacionalidade
-        self.email = email
-        self.vinculo = vinculo
-        self.aniversario = aniversario
-
-    def mostrar_contato(self):
-        print(f"""
-INFORMAÇÕES DO CONTATO:
-{self.nome}
-{self.telefone}
-{self.nacionalidade}
-{self.email}
-{self.vinculo}
-{self.aniversario}
-
-""")
+from classes import Contato
 
 
-    def novo_contato(self):
-        novo_nome = input("Nome:")
-        novo_telefone = input("Telefone:")
-        novo_nacional = input("Nacionalidade:")
-        novo_email = input("E-mail:")
-        novo_vinculo = input("Vínculo:")
-        novo_aniversario = input("Aniversário:")
-        
-        novo_ctt = Contato(novo_nome, novo_telefone, novo_nacional, novo_email, novo_vinculo, novo_aniversario)
+contato1 = Contato("Lay", "666", "brasileiro", "nseioquela@gmail", "Aluno", "23/09" )
 
-        return novo_ctt
+contato1.mostrar_contato
+
+novo_contato(self)
